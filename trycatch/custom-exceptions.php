215eaff11fc040defffce68dacfb841b7c883507
RetaxMaster <retaxmaster@hotmail.com>
2022-04-18 13:42:59 -0500

    10 Custom Exceptions

---
+++ trycatch/custom-exceptions.php
@@ -0,0 +1,43 @@
+<?php
+
+class MichiException extends Exception {
+
+    public function getMeow() {
+        return "Meow! 😾";
+    }
+    
+}
+
+
+class ConejiException extends Exception {
+
+    public function getRabbit() {
+        return "🐰";
+    }
+
+}
+
+try {
+
+    $exception = readline("Excepcion a lanzar: ");
+
+    if ($exception == "michi")
+        throw new MichiException("Michi incorrecto");
+    else
+        throw new ConejiException("Conejo incorrecto");
+    
+} catch (MichiException $e) {
+    
+    echo $e->getMessage() . "\n";
+    echo $e->getMeow();
+
+} catch (ConejiException $e) {
+    
+    echo $e->getMessage() . "\n";
+    echo $e->getRabbit();
+
+} finally {
+
+    echo "\n Te perdono :3 \n";
+
+}
