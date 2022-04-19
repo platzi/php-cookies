b30df6a3a4c4430a63a035bf8cb9f4eb1bf33ae0
RetaxMaster <retaxmaster@hotmail.com>
2022-04-19 11:23:21 -0500

    15 Retax Functions

---
+++ date/retax-functions.php
@@ -0,0 +1,10 @@
+<?php
+require("date.php");
+
+// echo get_date_from_timestamp("2022-04-25");
+// echo get_short_date_from_timestamp("2022-04-25");
+// echo get_time_from_timestamp("18:32:45");
+// echo get_day_of_the_week(4);
+// echo translate_time("1 día", false);
+// echo add_time("2022-04-23 12:45:34", "1 día + 2 horas + 1 mes");
+echo get_full_date("2022-03-14 16:35:45");
